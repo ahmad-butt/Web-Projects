9d8ff3177a77c722be6d248cb891ed8cd07b80e8
ahmad594 <hahahmad594@gmail.com>
2021-05-03 01:18:19 +0500

    ADD: header

---
+++ disney-plus/src/components/Header.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Nav>
+            <Logo src='/images/logo.svg'/>
+            <NavMenu>
+                <a>
+                    <img src='/images/home-icon.svg'/>
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src='/images/search-icon.svg'/>
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src='/images/watchlist-icon.svg'/>
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src='/images/original-icon.svg'/>
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src='/images/movie-icon.svg'/>
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src='/images/series-icon.svg'/>
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <UserImage src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'/>
+        </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.nav`
+    background-color: #090b13;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`
+
+const Logo = styled.img`
+    width: 80px;
+`
+
+const NavMenu = styled.div`
+    display: flex;
+    a{
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img{
+            height: 20px;
+        }
+        
+        span{
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            position: relative;
+
+            &:after{
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+            }
+        }
+
+        &:hover{
+            span:after{
+                transform: scaleX(1);
+                opacity: 1;
+            }
+        }
+    }
+    flex: 1;
+    cursor: pointer;
+`
+
+const UserImage = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+`
